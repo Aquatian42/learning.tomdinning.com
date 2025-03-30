0fd341d84fda89dd54f28564feb94728a0262c14
Aquatian42 <tomtomdin@gmail.com>
2025-03-30 21:38:17 +0100

    functions

--- Javascript/Functions/functions.js
+++ Javascript/Functions/functions.js
@@ -1,3 +1,18 @@
 function favoriteAnimal(animal) {
     return animal + " is my favorite animal!"
 }
+
+
+//  
+function add7(numberIn) {return numberIn + 7}
+//
+let multiply = (in1,in2) => in1*in2;
+//
+function capitalise(stringInput) {
+    let stringOutput = stringInput.charAt(0).toUpperCase() + stringInput.slice(1)
+    return stringOutput
+}
+//
+let lastLetter = word => word.slice(-1)
+//
+console.log(lastLetter("hello"))
