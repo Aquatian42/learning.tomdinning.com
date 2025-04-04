fbf3e164e572b53856ffa27b10c4d6d2749d6e6c
Aquatian42 <tomtomdin@gmail.com>
2025-04-04 23:04:34 +0100

    Small commit

--- Javascript/Arrays_and_Loops/Arrays_and_Loops.js
+++ Javascript/Arrays_and_Loops/Arrays_and_Loops.js
@@ -14,11 +14,8 @@ for (let i = 10;i > 0; i--){
 //Task2
 
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
-outputDiv = document.querySelector('.task2')
-const refused = document.createElement('p')
-const admitted = document.createElement('p')
-outputDiv.appendChild(admitted)
-outputDiv.appendChild(refused)
+const refused = document.querySelector('.refused')
+const admitted = document.querySelector('.admitted')
 for (person of people) {
     if (person == "Phil" || person == "Lola")  {
         refused.textContent += person + " ,"
@@ -29,6 +26,67 @@ for (person of people) {
     console.log(admitted.textContent)
 }
 
+//Loops1
+outputDiv = document.querySelector('.loops1');
+const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
+const list = document.createElement('ul');
+for (element of myArray){
+    const li = document.createElement('li');
+    li.textContent = element
+    list.appendChild(li)
+}
+outputDiv.appendChild(list)
+
+
+
+
+//Loops2
+
+const nameToSearch = 'Mustafa';
+const para = document.createElement('p');
+
+const phonebook = [
+    { name : 'Chris', number : '1549' },
+    { name : 'Li Kang', number : '9634' },
+    { name : 'Anne', number : '9065' },
+    { name : 'Francesca', number : '3001' },
+    { name : 'Mustafa', number : '6888' },
+    { name : 'Tina', number : '4312' },
+    { name : 'Bert', number : '7780' },
+    { name : 'Jada', number : '2282' },
+]
+
+for (let i = 0; i < phonebook.length;i++){
+    if (phonebook[i].name == nameToSearch){
+        para.textContent = `${phonebook[i].name}'s number is: ${phonebook[i].number}` 
+    }
+    console.log(para.textContent)
+}
+outputDiv = document.querySelector(".loops2");
+outputDiv.appendChild(para);
+
+
+
+//Loops 3
+outputDiv = document.querySelector(".loops3")
+const isPrime = (input) => {
+    let i = 2;
+    while (i < (input/2)){
+        if (input % i == 0){
+            return false
+        }  
+        i++
+    }
+    return true
+}
+const parag = document.createElement('p');
+for (let i = 500;i > 0;i--){
+    if (isPrime(i)) {
+        parag.textContent += `${i}, `
+    }
+}
+outputDiv.appendChild(parag);
 
 
 });
+
